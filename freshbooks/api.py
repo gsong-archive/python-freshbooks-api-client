3997de87f4dd75f313d6a202472859680f331182
George Song <george@monozuku.com>
2018-01-16 08:48:00 -0800

    Add support for more API calls

--- freshbooks/api.py
+++ freshbooks/api.py
@@ -6,43 +6,72 @@ import requests
 
 log = logging.getLogger(__name__)
 
-BASE_URL = 'https://api.freshbooks.com'
-HEADERS = {
-    'Authorization': 'Bearer <Bearer Token>',
-    'Api-Version': 'alpha',
-    'Content-Type': 'application/json'
-}
-
 
 @attr.s(hash=True)
 class Client:
     __base_url__ = 'https://api.freshbooks.com'
-    __headers__ = attr.ib(init=False, repr=False, hash=False)
-
     token = attr.ib(repr=False)
 
-    def __attrs_post_init__(self):
-        self.__headers__ = {
-            'Authorization': f"Bearer {self.token}",
-            'Api-Version': 'alpha',
-            'Content-Type': 'application/json'
-        }
+    @property
+    def business_roles_identity(self) -> dict:
+        endpoint = '/auth/api/v1/users/me'
+        res = self._get(endpoint)
+        return res['response']
 
-    def _get(self, endpoint: str, params: dict) -> dict:
+    def list_clients(self, account_id: str, filters: dict=None) -> dict:
+        endpoint = f"/accounting/account/{account_id}/users/clients"
+        res = self._filtered_get_with_content_type(endpoint, filters)
+        return res['response']['result']
+
+    def list_projects(self, business_id: int, filters: dict=None) -> dict:
+        endpoint = f"/projects/business/{business_id}/projects"
+        return self._filtered_get(endpoint, filters)
+
+    def fetch_time_entries(self, business_id: int, filters: dict=None) -> dict:
+        endpoint = f"/timetracking/business/{business_id}/time_entries"
+        return self._filtered_get(endpoint, filters)
+
+    def _get(
+            self,
+            endpoint: str,
+            headers: dict=None,
+            params: dict=None,
+    ) -> dict:
+        if headers is None:
+            headers = self.__headers_with_content_type__
+
         url = f"{self.__base_url__}{endpoint}"
-        res = requests.get(url, headers=self.__headers__, params=params)
+        res = requests.get(url, headers=headers, params=params)
         log.debug(res.url)
-        return res.json()['response']['result']
+        print(res.url)
+        return res.json()
+
+    def _filtered_get(self, endpoint: str, filters: dict=None) -> dict:
+        return self._get(endpoint, self.__headers__, filters)
+
+    def _filtered_get_with_content_type(
+            self,
+            endpoint: str,
+            filters: dict=None,
+    ) -> dict:
+        if filters:
+            searches = {f"search[{k}]": v for k, v in filters.items()}
+        return self._get(
+            endpoint, self.__headers_with_content_type__, searches
+        )
 
     @property
     @lru_cache()
-    def business_roles_identity(self) -> dict:
-        endpoint = '/auth/api/v1/users/me'
-        return self._get(endpoint)
+    def __headers__(self):
+        return {
+            'Authorization': f"Bearer {self.token}",
+            'Api-Version': 'alpha',
+        }
 
+    @property
     @lru_cache()
-    def list_clients(self, account_id: str, **filters) -> dict:
-        endpoint = f"/accounting/account/{account_id}/users/clients"
-        if filters:
-            searches = {f"search[{k}]": v for k, v in filters.items()}
-        return self._get(endpoint, searches)
+    def __headers_with_content_type__(self):
+        return {
+            'Content-Type': 'application/json',
+            **self.__headers__
+        }
